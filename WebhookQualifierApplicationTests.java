05366c824b30a634bcdc13a43ec5411103c5bc12
vedraut00 <135373299+vedraut00@users.noreply.github.com>
2025-08-29 10:45:04 +0530

    Create WebhookQualifierApplicationTests.java

---
+++ WebhookQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajajfinserv.webhookqualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookQualifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
